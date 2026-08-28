1a91de9837b48350f41a79e8d7740422acc4e234
coderrvs-pixel <coderrvs@gmail.com>
2026-08-28 13:31:12 +0000

    Restore receiver cake and theme sync

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const selectedThemeColor = new URLSearchParams(window.location.search).get("colo
 if (/^#[0-9a-f]{6}$/i.test(selectedThemeColor || "")) {
     document.documentElement.style.setProperty("--theme-color", selectedThemeColor);
     document.documentElement.style.setProperty("--theme-contrast", "#ffffff");
+    document.documentElement.style.setProperty("--accent", selectedThemeColor);
 }
